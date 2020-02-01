6652b7ade5e1612972f6f6b34e0437788cfb337f
Jeroen Schickendantz <60529194+Schickendantzj@users.noreply.github.com>
2020-02-01 08:27:34 -0800

    Update CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,11 +6,15 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 0, -20);
-
+    public float leftBound = -100;
     // Update is called once per frame
     void Update()
     {
         //Used to follow player object
-        gameObject.transform.position = player.transform.position + offset;
+        if ((player.transform.position + offset).x > leftBound)
+        {
+            gameObject.transform.position = player.transform.position + offset;
+        }
+        
     }
 }
