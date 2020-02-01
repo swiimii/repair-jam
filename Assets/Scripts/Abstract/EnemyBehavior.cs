3df01134289f9d877e9ca4cd7510feef2b70725e
Samuel Scherer <scheresr@mail.uc.edu>
2020-01-31 22:30:15 -0500

    Added some tags, and added base enemy behavior

---
+++ Assets/Scripts/Abstract/EnemyBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EnemyBehavior : MonoBehaviour
+{
+    public abstract void Move(Vector2 dir);
+
+    public abstract void Damage(int dmg);
+
+    public abstract void Death();
+}
