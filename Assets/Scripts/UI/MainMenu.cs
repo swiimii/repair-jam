41a56013c1e5fcd332488b7904c44676d3eeb8cf
WillCPP <WillCPP@users.noreply.github.com>
2020-01-31 22:53:55 -0500

    Add home menu scene, intro scene, intro animation

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        //SceneManager.LoadScene("");  // Uncomment this line and add the first scene
+    }
+
+    public void Exit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
