d4b5239ea466e18fd6d03191ed58249c25848473
Samuel Scherer <scheresr@mail.uc.edu>
2020-02-01 09:03:13 -0500

    Made changes for player health
    Health controller, with interactions ready for player movement

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int health = 3;
+    [SerializeField] bool invulnerable = false;
+    [SerializeField] float invulnerablilityTime = .3f;
+
+    public void Damage(int dmg)
+    {
+        if(!invulnerable)
+        {
+            health -= dmg;
+            // GetComponent<MovementBehavior>().knockBack();
+            StartCoroutine("Invulnerable");
+        }
+    }
+
+    public bool IsInvulnerable()
+    {
+        return invulnerable;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
+
+    public IEnumerator Invulnerable()
+    {
+        invulnerable = true;
+        yield return new WaitForSeconds(invulnerablilityTime);
+        invulnerable = false;
+        yield return null;
+    }
+}
