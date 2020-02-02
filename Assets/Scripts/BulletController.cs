f34ec099215946de340f6bd495a5e29380c17de8
Jeroen Schickendantz <60529194+Schickendantzj@users.noreply.github.com>
2020-02-02 10:13:10 -0800

    Added Apple Enemy

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float direction = 0;
+    public float movespeed = .5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("EndMe");
+    }
+
+    public void FixedUpdate()
+    {
+        GetComponent<BasicEnemyBehavior>().Move(direction * Vector2.right * movespeed);
+    }
+    public IEnumerator EndMe()
+    {
+        yield return new WaitForSeconds(7f);
+        Destroy(gameObject);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("collided");
+        if (collision.gameObject.layer == 9)
+        {
+            print("meme");
+            collision.gameObject.GetComponent<PlayerHealth>().Damage(1);
+        }
+    }
+}
