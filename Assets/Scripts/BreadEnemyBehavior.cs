27459bb2a62b8993a770ec28dddd25c8195c951b
WillCPP <WillCPP@users.noreply.github.com>
2020-02-02 03:29:55 -0500

    Start: Animate bread

---
+++ Assets/Scripts/BreadEnemyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadEnemyBehavior : EnemyBehavior
+{
+    //public void Move(Vector3 direction)
+    //{
+    //    transform.position += direction;
+    //}
+
+    public override void Move(Vector2 direction)
+    {
+        transform.position += (Vector3)direction;
+    }
+
+    public virtual void Knockback(Vector2 direction)
+    {
+        return;
+    }
+}
+       
